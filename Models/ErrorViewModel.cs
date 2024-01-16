d868616d30366b9677a76adf3ce1c6d100739ace
Ivan Vieira <iivnn00@gmail.com>
2024-01-16 20:25:17 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuestionsAndAnswers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
